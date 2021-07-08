2f49805f082328476e4ee7e01b9a090db215c13a
MH15O <76598767+MH15O@users.noreply.github.com>
2021-07-08 01:27:41 +0100

    Add files via upload

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import "./assets/Todolist.css";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      '& > * + *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
+
+function HomeIcon(props) {
+    return (
+      <SvgIcon {...props}>
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+      </SvgIcon>
+    );
+  }
+
+const SimpleAlert = ({severity , text}) => {
+    const classes = useStyles();
+    return(
+        <div className = {classes.root}>
+            <Alert severity={severity}>
+            <AlertTitle>Oops an Error have occured</AlertTitle>
+                {text}
+            </Alert>
+        </div>
+    );
+};
+
 const Todolist = () => {
+    const classes = useStyles();
     const [statue , setStatue] = React.useState("");
     let [tasks , setTasks] = React.useState([]);
     let stringTasks = tasks.toString();
@@ -11,9 +45,12 @@ const Todolist = () => {
         if(item.length > 0)
         {
             setTasks(tasks.concat(item));
+            <SimpleAlert severity="success" text = "SuccessFully added"/>
             setItem("");
         }else{
-            setStatue("You Can't Add an empty Element");
+            <div className={classes.root}>
+                <SimpleAlert severity="error" text="You Can't add and empty element"/>
+            </div>
             
         }
     };
@@ -37,9 +74,15 @@ const Todolist = () => {
             <br/><br/>
             <label htmlFor="title" className="labels">Add Your Task</label>
             <br/><br/>
-            <input type = "text" 
-            placeholder = "Your Task"
-            onChange = {(e) => setItem(e.target.value)}></input>
+            <div className = "inputs">
+                <div className="realInput">
+                <a className="unique" onClick = {() => {console.log("clicked")}}><HomeIcon/></a>
+                <input type = "text" 
+                placeholder = "Your Task"
+                onChange = {(e) => setItem(e.target.value)}>
+                </input>
+                </div>
+            </div>
             <br/>
             <button onClick={addTask}>Add Task</button>
             <p className = "statue"
